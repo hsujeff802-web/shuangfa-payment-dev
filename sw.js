9b75ec3831ebbcd9bb923e39bfe8a5cb5d84c405
hsujeff802-web <hsujeff802@gmail.com>
2026-07-29 10:18:58 +0800

    V8.1_支票票號格式修正版

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME='shuangfa-payment-v7.0-official-1';
+const CACHE_NAME='shuangfa-payment-v8.1-check-format-1';
 const APP_SHELL=[
   './',
   './index.html',
-  './styles.css?v=80',
-  './app.js?v=80',
+  './styles.css?v=81',
+  './app.js?v=81',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
