5394a187a59883c8a83289b41141fdd7116a9b6d
hsujeff802-web <hsujeff802@gmail.com>
2026-07-29 12:44:06 +0800

    雙發付款管理系統_V8.3_DEV_Build005_儲存空間修正版

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE_NAME='shuangfa-payment-v8.3-dev-build004-8307';
+const CACHE_NAME='shuangfa-payment-v8.3-build005-storage';
 const APP_SHELL=[
   './',
   './index.html',
-  './styles.css?v=8307',
-  './app.js?v=8307',
-  './v83.js?v=8307',
+  './styles.css?v=8305',
+  './app.js?v=8305',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
